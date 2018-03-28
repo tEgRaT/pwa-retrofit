7191f9f288d5947ee6779fce5964d8da8d9a4b69
tEgRaT <tEgRaT@users.noreply.github.com>
2018-03-28 10:46:02 +0800

    modify installFilesEssential

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ const
   CACHE = version + '::PWAsite',
   offlineURL = '/offline/',
   installFilesEssential = [
-    '/',
+    '/eassist',
     '/manifest.json',
     '/css/styles.css',
     '/js/main.js',
